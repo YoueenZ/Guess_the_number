8f363a1d19685cc9646869a6ecdffd408dbb8094
YoueenZ <92889244+YoueenZ@users.noreply.github.com>
2021-11-08 15:27:32 +0000

    Update module.py

--- module.py
+++ module.py
@@ -1,8 +1,10 @@
-# seperate the integer
+import random
 def seperate_int(number):   
     x = [int(a) for a in str(number)]
     return x
 def return_element(a):
     x = a[0]
     return x
-
+def create_random_num(a,b):
+    x = int(random.randrange(a, b))
+    return x
